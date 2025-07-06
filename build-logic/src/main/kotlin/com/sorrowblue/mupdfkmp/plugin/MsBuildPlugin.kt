cb824d30e12bfae6a9905bd17b4fc66420af1da7
SorrowBlue <sorrowblue.dev@gmail.com>
2025-07-07 03:31:25 +0900

    add tasks for building and copying MuPDF libraries for Windows and Linux platforms

--- build-logic/src/main/kotlin/com/sorrowblue/mupdfkmp/plugin/MsBuildPlugin.kt
+++ build-logic/src/main/kotlin/com/sorrowblue/mupdfkmp/plugin/MsBuildPlugin.kt
@@ -1,141 +1,127 @@
 package com.sorrowblue.mupdfkmp.plugin
 
-import java.security.MessageDigest
-import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.DuplicatesStrategy
-import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.Exec
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputDirectory
-import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.OutputFile
-import org.gradle.api.tasks.TaskAction
 import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.register
 
 class MsBuildPlugin : Plugin<Project> {
     override fun apply(target: Project) {
+        with(target) {
+            // Windows x86
+            tasks.register<MsBuildTask>("buildJavaviewerlibWindowsX86") {
+                group = "build"
+                description = "Builds the native javaviewerlib using MSBuild."
+                workingDirectory.set(layout.projectDirectory.dir("../mupdf/platform/win32/"))
+                projectName.set("javaviewerlib.vcxproj")
+                configuration.set("Release")
+                platform.set("x86")
+            }
+            tasks.register<Copy>("copyJavaviewerlibDll") {
+                mustRunAfter("buildJavaviewerlibWindowsX86")
+                group = "build"
+                description = "Copies javaviewerlib64.dll to the resources folder."
+                from(file("../mupdf/platform/win32/Release/javaviewerlib.dll"))
+                into(layout.projectDirectory.dir("src/jvmMain/resources/Windows-x84"))
+                rename { "mupdf_java.dll" }
+                duplicatesStrategy = DuplicatesStrategy.INCLUDE
+            }
+            tasks.register<GenerateMupdfLibPropertiesTask>("generateMupdfLibPropertiesWindowsX86") {
+                mustRunAfter("copyJavaviewerlibDll")
+                libVersion = version.toString()
+                dllFile = layout.projectDirectory.file("src/jvmMain/resources/Windows-x84/mupdf_java.dll")
+                propsFile = layout.projectDirectory.file("src/jvmMain/resources/Windows-x84/mupdf-lib.properties")
+            }
+            tasks.register("desktopResourceWindowsX86") {
+                group = "build"
+                description = "Windows x86 resources for desktop."
+                dependsOn(
+                    "buildJavaviewerlibWindowsX86",
+                    "copyJavaviewerlibDll",
+                    "generateMupdfLibPropertiesWindowsX86"
+                )
+            }
 
-        target.tasks.register<MsBuildTask>("buildJavaviewerlibx86") {
-            group = "build"
-            description = "Builds the native javaviewerlib using MSBuild."
-            workingDirectory.set(target.layout.projectDirectory.dir("../mupdf/platform/win32/"))
-            projectName.set("javaviewerlib.vcxproj")
-            configuration.set("Release")
-            platform.set("x86")
-            finalizedBy("copyJavaviewerlibDll")
+            // Windows amd64
+            tasks.register<MsBuildTask>("buildJavaviewerlibWindowsAMD64") {
+                group = "build"
+                description = "Builds the native javaviewerlib using MSBuild."
+                workingDirectory.set(layout.projectDirectory.dir("../mupdf/platform/win32/"))
+                projectName.set("javaviewerlib.vcxproj")
+                configuration.set("Release")
+                platform.set("x64")
+            }
+            tasks.register<Copy>("copyJavaviewerlib64Dll") {
+                mustRunAfter("buildJavaviewerlibWindowsAMD64")
+                group = "build"
+                description = "Copies javaviewerlib64.dll to the resources folder."
+                from(file("../mupdf/platform/win32/x64/Release/javaviewerlib64.dll"))
+                into(layout.projectDirectory.dir("src/jvmMain/resources/Windows-amd64"))
+                rename { "mupdf_java.dll" }
+                duplicatesStrategy = DuplicatesStrategy.INCLUDE
+            }
+            tasks.register<GenerateMupdfLibPropertiesTask>("generateMupdfLibPropertiesWindowsAMD64") {
+                mustRunAfter("copyJavaviewerlib64Dll")
+                libVersion = version.toString()
+                dllFile =
+                    layout.projectDirectory.file("src/jvmMain/resources/Windows-amd64/mupdf_java.dll")
+                propsFile =
+                    layout.projectDirectory.file("src/jvmMain/resources/Windows-amd64/mupdf-lib.properties")
+            }
+            tasks.register("desktopResourceWindowsAMD64") {
+                group = "build"
+                description = "Windows amd64 resources for desktop."
+                dependsOn(
+                    "buildJavaviewerlibWindowsAMD64",
+                    "copyJavaviewerlib64Dll",
+                    "generateMupdfLibPropertiesWindowsAMD64"
+                )
+            }
+
+            tasks.register<AddUft8BomToFilesTask>("addUtf8BomToFiles") {
+                group = "other"
+                description = "Overwrite and save multiple UTF-8 BOM-free files with UTF-8 BOM."
+                inputFiles.setFrom(
+                    file("../mupdf/thirdparty/zxing-cpp/core/src/BitMatrixIO.cpp"),
+                    file("../mupdf/thirdparty/zxing-cpp/core/src/WriteBarcode.cpp")
+                )
+            }
+
+            // Linux amd64
+            tasks.register<Exec>("buildLibmupdfLinuxAMD64") {
+                group = "build"
+                description = "Builds libmupdf_java64.so"
+                commandLine("make", "java",)
+            }
+            tasks.register<Copy>("copyLibmupdfJava64So") {
+                mustRunAfter("buildLibmupdfLinuxAMD64")
+                group = "build"
+                description = "Copies libmupdf_java64.so to the resources folder."
+                from(file("../mupdf/build/java/release/libmupdf_java64.so"))
+                into(layout.projectDirectory.dir("src/jvmMain/resources/Linux-amd64"))
+                rename { "mupdf_java.so" }
+                duplicatesStrategy = DuplicatesStrategy.INCLUDE
+            }
+            tasks.register<GenerateMupdfLibPropertiesTask>("generateMupdfLibPropertiesLinuxAMD64") {
+                mustRunAfter("copyLibmupdfJava64So")
+                libVersion = version.toString()
+                dllFile =
+                    layout.projectDirectory.file("src/jvmMain/resources/Linux-amd64/mupdf_java.so")
+                propsFile =
+                    layout.projectDirectory.file("src/jvmMain/resources/Linux-amd64/mupdf-lib.properties")
+            }
+            tasks.register("desktopResourceLinuxAMD64") {
+                group = "build"
+                description = "Windows amd64 resources for desktop."
+                dependsOn(
+                    "buildLibmupdfLinuxAMD64",
+                    "copyLibmupdfJava64So",
+                    "generateMupdfLibPropertiesLinuxAMD64"
+                )
+            }
         }
-        target.tasks.register<MsBuildTask>("buildJavaviewerlibx64") {
-            group = "build"
-            description = "Builds the native javaviewerlib using MSBuild."
-            workingDirectory.set(target.layout.projectDirectory.dir("../mupdf/platform/win32/"))
-            projectName.set("javaviewerlib.vcxproj")
-            configuration.set("Release")
-            platform.set("x64")
-            finalizedBy("copyJavaviewerlib64Dll")
-        }
-
-        target.tasks.register<Copy>("copyJavaviewerlib64Dll") {
-            dependsOn("buildJavaviewerlibx64")
-            group = "build"
-            description = "Copies javaviewerlib64.dll to the resources folder."
-            from(target.file("../mupdf/platform/win32/x64/Release/javaviewerlib64.dll"))
-            into(target.layout.projectDirectory.dir("src/jvmMain/resources/Windows-amd64"))
-            rename { "mupdf_java.dll" }
-            duplicatesStrategy = DuplicatesStrategy.INCLUDE
-            finalizedBy("generateMupdfLibProperties-amd64")
-        }
-        target.tasks.register<Copy>("copyJavaviewerlibDll") {
-            dependsOn("buildJavaviewerlibx86")
-            group = "build"
-            description = "Copies javaviewerlib64.dll to the resources folder."
-            from(target.file("../mupdf/platform/win32/Release/javaviewerlib.dll"))
-            into(target.layout.projectDirectory.dir("src/jvmMain/resources/Windows-x84"))
-            rename { "mupdf_java.dll" }
-            duplicatesStrategy = DuplicatesStrategy.INCLUDE
-            finalizedBy("generateMupdfLibProperties-x84")
-        }
-
-
-        target.tasks.register<GenerateMupdfLibPropertiesTask>("generateMupdfLibProperties-amd64") {
-            this.libVersion = target.version.toString()
-            this.dllFile =
-                target.layout.projectDirectory.file("src/jvmMain/resources/Windows-amd64/mupdf_java.dll")
-            this.propsFile =
-                target.layout.projectDirectory.file("src/jvmMain/resources/Windows-amd64/mupdf-lib.properties")
-        }
-        target.tasks.register<GenerateMupdfLibPropertiesTask>("generateMupdfLibProperties-x84") {
-            this.libVersion = target.version.toString()
-            this.dllFile =
-                target.layout.projectDirectory.file("src/jvmMain/resources/Windows-x84/mupdf_java.dll")
-            this.propsFile =
-                target.layout.projectDirectory.file("src/jvmMain/resources/Windows-x84/mupdf-lib.properties")
-        }
-
-        target.tasks.register<AddUft8BomToFilesTask>("addUtf8BomToFiles") {
-            group = "build"
-            description = "複数のUTF-8 BOMなしファイルをUTF-8 BOM付きで上書き保存する。"
-            inputFiles.setFrom(
-                target.file("../mupdf/thirdparty/zxing-cpp/core/src/BitMatrixIO.cpp"),
-                target.file("../mupdf/thirdparty/zxing-cpp/core/src/WriteBarcode.cpp")
-            )
-        }
-    }
-}
-
-abstract class MsBuildTask : Exec() {
-
-    @get:InputDirectory
-    abstract val workingDirectory: DirectoryProperty
-
-    @get:Input
-    abstract val projectName: Property<String>
-
-    @get:Input
-    abstract val configuration: Property<String>
-
-    @get:Input
-    abstract val platform: Property<String>
-
-    @TaskAction
-    fun task() {
-        workingDir = workingDirectory.get().asFile
-        commandLine(
-            "MsBuild.exe",
-            projectName.get(),
-            "/p:configuration=${configuration.get()}",
-            "/p:platform=${platform.get()}"
-        )
-    }
-}
-
-abstract class GenerateMupdfLibPropertiesTask : DefaultTask() {
-
-    @get:InputFile
-    abstract val dllFile: RegularFileProperty
-
-    @get:Input
-    abstract val libVersion: Property<String>
-
-    @get:OutputFile
-    abstract val propsFile: RegularFileProperty
-
-    @TaskAction
-    fun generateProperties() {
-        val hash = MessageDigest.getInstance("SHA-256")
-            .digest(dllFile.get().asFile.readBytes())
-            .joinToString("") { "%02x".format(it) }
-        propsFile.get().asFile.writeText(
-            """
-            build.ref=${libVersion.get()}
-            lib.name=${dllFile.get().asFile.name}
-            lib.hash=$hash
-            """.trimIndent()
-        )
     }
 }
